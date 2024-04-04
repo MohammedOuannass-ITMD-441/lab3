02e020dc2b772b73217324202d73f7aead65b7c8
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2024-04-03 22:50:18 -0500

    error handling + finishing touches

--- script.js
+++ script.js
@@ -3,19 +3,29 @@ const tipSlider = document.getElementById('tipSlider');
 const tipPercent = document.getElementById('tipPercent');
 const tipAmount = document.getElementById('tipAmount');
 const totalAmount = document.getElementById('totalAmount');
-const errorOutput = document.getElementById('errorOutput');
+const errorOutput = document.getElementById('error');
+
+function validateInput(input) {
+    if (!input.checkValidity()) {
+        errorOutput.textContent = 'Please enter a valid positive number for the bill total.';
+    }else{
+        errorOutput.textContent = '';
+    }
+}
 
 function calculateTip() {
     let bill = parseFloat(billTotal.value);
     let tip = tipSlider.value;
-    if (!isNaN(bill)) {
+    if (bill >= 0) {
         let tipValue = bill * (tip / 100);
         let totalBill = bill + tipValue;
         tipAmount.value = tipValue.toFixed(2);
         totalAmount.value = totalBill.toFixed(2);
         errorOutput.textContent = '';
     } else {
-        errorOutput.textContent = 'Please enter a valid number for the bill total.';
+        tipAmount.value = '';
+        totalAmount.value = '';
+        errorOutput.textContent = 'Please enter a valid positive number for the bill total.';
     }
 }
 
