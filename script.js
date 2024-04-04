3a228ddc3632e26d6a1f18deab892463eb159f42
MohammedOuannass <120182825+MohammedOuannass@users.noreply.github.com>
2024-04-03 21:44:49 -0500

    adding const script

---
+++ script.js
@@ -0,0 +1,7 @@
+const billTotal = document.getElementById('billTotal');
+const tipSlider = document.getElementById('tipSlider');
+const tipPercent = document.getElementById('tipPercent');
+const tipAmount = document.getElementById('tipAmount');
+const totalAmount = document.getElementById('totalAmount');
+const errorOutput = document.getElementById('errorOutput');
+
